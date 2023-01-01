9a0efc96c08cdcaa867377662db9a2b7f3eed4eb
Opeyemil-code <opeyemitoheeb31@gmail.com>
2022-12-31 12:23:25 -1200

    start today

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ function Dark(params) {
     document.body.classList.add('bg-blackblue');
     searchInput.classList.remove('bg-white');
     searchInput.classList.add('bg-vdarkgrayblue');
+    searchInput.classList.add('text-white');
     todoContainer.classList.add('text-white');
     dragAndDropMsg.classList.add('text-white');
     mobileDiv.classList.add('bg-vdarkgrayblue');
@@ -68,45 +69,56 @@ lightMode.addEventListener(
 const todo_wrapper = document.getElementById('todo');
 const newTodo = document.getElementById('search');
 const toDo = []
+const countingNum = document.getElementById('counter')
 
+
+let counter = 5
 function addToDo(params) {
-
-    if (toDo.length > 0) {
-        for (let b = 0; b < toDo.length; b++) {
-            let checkingbox = document.createElement('input')
-            checkingbox.type = "checkbox";
-           // checkingbox.className = "flex absolute left-0"
-          todo_wrapper.appendChild(checkingbox)
-        }
-    }
-
-
-
+    
+ //THIS CREATED ANOTHER DIV BOX ON THE WEB PAGE THORUGH JS   
    if (newTodo.value != '') {
-      toDo.push(newTodo.value)
+    toDo.push(newTodo.value)
 
-      let newTodo_list = document.createElement('div')
-      newTodo_list.className = "flex flex-row  border-b  border-b-gray-400  w-96 text-xl space-x-4 pb-2 pt-2 rounded-t-md  bg-whit text-center mx-auto"
+    let newTodo_list = document.createElement('div')
+    newTodo_list.className = "flex flex-row  border-b  border-b-gray-400  w-96 text-xl space-x-4 pb-2 pt-2 rounded-t-md  bg-whit text-center mx-auto justify-center"
 
-        for (let i = 0; i < toDo.length; i++) {
-        newTodo_list.innerHTML = newTodo.value;
-        todo_wrapper.appendChild(newTodo_list);  
-      }
+      for (let i = 0; i < toDo.length; i++) {
+      newTodo_list.innerHTML = newTodo.value;
+      todo_wrapper.appendChild(newTodo_list);
+ }
 
 
-      if (toDo.length > 0) {
+ //THIS IS USED TO BUILD ANOTHER DIV BUTTON
+     if (toDo.length > 0) {
        for (let j = 0; j < toDo.length; j++) {
-            let delete_todo = document.createElement('div')
+            let delete_todo = document.createElement('button')
          delete_todo.className = "flex  right-1 absolute lg:hidden lg:text-black text-gray-400"
          delete_todo.innerHTML = "X"
           newTodo_list.appendChild(delete_todo)  
         }
       }
 
-      
-      }
 
-     }
+ //THIS IS USED TO CREATE A CHECKBOX ON THE HTML WEB PAGE    
+      if (toDo.length > 0) {
+        for (let b = 0; b < toDo.length; b++) {
+            let checkingbox = document.createElement('input')
+            checkingbox.type = "checkbox";
+           checkingbox.className = "flex absolute left-0"
+          newTodo_list.appendChild(checkingbox)
+        }
+    }
+
+   
+
+    }
+
+    counter--
+    countingNum.innerHTML = counter
+      
+}
+
+     
 
      
 
